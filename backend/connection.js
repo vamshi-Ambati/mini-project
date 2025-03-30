e68895c8f348cb6729844def6aedacb080d31ed7
vamshi-Ambati <vamshiambati27@gmail.com>
2025-03-30 12:51:54 +0530

    final commit

--- backend/connection.js
+++ backend/connection.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
 
 const connectMongoDb = async () => {
-    await mongoose.connect("mongodb://localhost:27017/mini-project")
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.error(err));
-}
+    try {
+        const mongoUrl = process.env.MONGO_URL; // Get from environment variable
+        if (!mongoUrl) {
+            console.error("MONGO_URL environment variable is not set.");
+            return; // Exit if the variable is missing
+        }
+
+        await mongoose.connect(mongoUrl)
+        .then(() => console.log("Connected to MongoDB")  ).
+        catch((err) => console.error("Error connecting to MongoDB:", err));
+    } catch (err) {
+        console.error("Error connecting to MongoDB:", err);
+    }
+};
 
 module.exports = {
-    connectMongoDb
-}
+    connectMongoDb,
+};
