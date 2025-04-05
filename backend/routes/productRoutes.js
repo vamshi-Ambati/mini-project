317d118008e51f844b92fb588b4a3bc2b3baa0fc
vamshi-Ambati <vamshiambati27@gmail.com>
2025-04-05 09:26:14 +0530

    initial comit

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,15 +1,19 @@
-const express = require('express');
-const {addProduct , getProductByFirm,deleteProductById} = require('../controllers/productController')
+const express = require("express");
+const {
+  addProduct,
+  getProductByFirm,
+  deleteProductById,
+} = require("../controllers/productController");
 
 const router = express.Router();
 
-router.post('/add-product/:firmId',addProduct)
-router.get("/:id/products", getProductByFirm)
+router.post("/add-product/:firmId", addProduct); // Correct singular 'product'
+router.get("/:id/products", getProductByFirm);
 router.get("/uploads/:imageName", (req, res) => {
-    const imageName = req.params.imageName;
-    res.headersSent("Content-Type", "image/jpeg");
-    res.sendFile(path.join(__dirname, "..", "uploads", imageName));
-})
+  const imageName = req.params.imageName;
+  res.headersSent("Content-Type", "image/jpeg");
+  res.sendFile(path.join(__dirname, "..", "uploads", imageName));
+});
 
-router.delete("/delete-product/:id", deleteProductById)
+router.delete("/delete-product/:id", deleteProductById);
 module.exports = router;
